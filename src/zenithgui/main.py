b0314ccf5af1cc8493a2f519c6f18deab6c5e4fe
vinicius-arg <j.vin7cius@gmail.com>
2025-05-03 22:47:13 -0300

    feat: Interface PyQt6

--- src/zenithgui/main.py
+++ src/zenithgui/main.py
@@ -1,7 +1,16 @@
+import sys
+
 from zenithgui import config
+from PyQt6.QtWidgets import QApplication
+
+from view.main_window import MainWindow
 
 def main():
-    ...
+    app = QApplication(sys.argv)
+
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
